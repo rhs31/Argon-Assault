d5450af07493f47c5d4fa61f007a7b9c8e0585a7
rhs31 <r.silverberg@yahoo.com>
2018-08-13 12:33:01 -0400

    Added Terrain, Ship, Circuit, Started Movement Script

---
+++ Argon Assault Project/Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("In meters per second")][SerializeField] float xSpeed = 4f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //how far is the gamepad stick thrown to one side (+) or the other side (-) (goes between 0 and 1)
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+        print(xOffsetThisFrame);
+	}
+}
