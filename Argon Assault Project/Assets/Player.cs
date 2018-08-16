26feab70136ce1e20d04c1906e486def6fea154e
rhs31 <r.silverberg@yahoo.com>
2018-08-16 12:58:53 -0400

    Added pitch, yaw, roll

--- Argon Assault Project/Assets/Player.cs
+++ Argon Assault Project/Assets/Player.cs
@@ -1,21 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-    [Tooltip("In meters per second")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In meters per second")][SerializeField] float speed = 10f;
+    [Tooltip("In meters per second")] [SerializeField] float xRange = 5f;
+    [Tooltip("In meters per second")] [SerializeField] float yRange = 5f;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField] float positionPitchFactor = -5f; //what you multiply position by to get pitch
+    [SerializeField] float controlPitchFactor = -20f;
+
+    [SerializeField] float positionYawFactor = 5f;
+
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow, yThrow;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //how far is the gamepad stick thrown to one side (+) or the other side (-) (goes between 0 and 1)
-        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
-        print(xOffsetThisFrame);
-	}
+	void Update ()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll); //DO NOT SET localRotation.x directly, it is quaternion, NOT Euler. Use this pattern instead.
+
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //how far is the gamepad stick thrown to one side (+) or the other side (-) (goes between 0 and 1)
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * speed * Time.deltaTime; //how many meters to move in this frame
+        float yOffset = yThrow * speed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset; //clamp x position
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float rawYPos = transform.localPosition.y + yOffset; //clamp y position
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z); //don't change z position
+    }
 }
